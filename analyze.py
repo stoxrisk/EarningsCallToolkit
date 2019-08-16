174f4d3869b972aa5e86e7686d0dba6518af0482
jackoaw <worldchuba@gmail.com>
2019-08-16 15:11:39 -0400

    before computer restart for api key

--- analyze.py
+++ analyze.py
@@ -1,12 +1,14 @@
 from yahoofinancials import YahooFinancials
 from tdameritradeapi import TDAmeritradeAPI
 from calendarparser import CalendarParser
-from datetime import timedelta, date
+from datetime import timedelta, date, datetime
 import matplotlib.pyplot as plts
 import requests
 import os
 
 api_key = os.getenv('td_api_key')
+
+print(api_key)
 
 # Return info as map
 
@@ -28,20 +30,19 @@ class LineGraph:
 # RETURN: [1,2,2,2]
 class IntraDayToolkit:
 	def __init__(self, api_key):
-		print("sda")
 		self.td_api = TDAmeritradeAPI(api_key)
-		print("sda2")
-		
+
 	def retrieveSelectedTimesDifferences(self, dates, times, ticker, candlestick):
-		start_date = datetime.strptime(dates[0], '%Y%m%d')
-		end_date = datetime.strptime(dates[len(dates)-1], '%Y%m%d')
 		# Translate candlestick
 		if candlestick == "30m":
 			freq_type = "minute"
 			freq_num = "30"
 		# Convert to miliseconds for api call
-		start_date = start_date.timestamp() * 1000
-		end_date = end_date.timestamp() * 1000
+		start_date = dates[0].timestamp() * 1000
+		end_date = dates[len(dates)-1].timestamp() * 1000
+		print(start_date)
+		# start_date = dates[0].strftime('%Y%m%d')
+		# end_date = dates[len(dates)-1].strftime('%Y%m%d')
 		# Get all the data for this ticker
 		data_return = td_api.getPrices(ticker, freq_type, freq_num, start_date, end_date).content
 		print(data_return)
@@ -57,18 +58,21 @@ def afterMarketCallDifferenceStrategy(earnings_map, ticker_list):
 	tk = IntraDayToolkit(api_key)
 	after_market_strat_times = ["9:30-16,16-20", "4-9:30,9:30-16"]
 	for ticker in ticker_list:
-		for earningsdate in earningdate:
+		earnings_date_list = cp.earnings_map[ticker]
+		for earningsdate in earnings_date_list:
+			print(earningsdate)
 			dateandtime = earningsdate.split(":")
 			# We only want to pay attention to stocks with after market close
-			if(dateandtime[1] is not "amc"):
+			if(dateandtime[1] != "amc"):
 				return
 			# Setup dates
-			start_date = datetime.strptime(earningsdate[0], '%Y%m%d')
-			end_date = datetime.strptime(earningsdate[0], '%Y%m%d')
-			end_date = start_date + datetime.timedelta(days=1) # last day is not inclusive
+			print(dateandtime[0])
+			start_date = datetime.strptime(dateandtime[0], '%Y%m%d')
+			end_date = datetime.strptime(dateandtime[0], '%Y%m%d')
+			end_date = start_date + timedelta(days=1, hours=20) # last day is not inclusive
 			dates = [start_date, end_date]
 			# Expected return [1.1,2.1,-1,2] in percentages
-			retrieveSelectedTimesDifferences(dates, after_market_strat_times, ticker, "30m")
+			tk.retrieveSelectedTimesDifferences(dates, after_market_strat_times, ticker, "30m")
 			# x_axis = ["intraday1", "aftermarket", "premarket", "intraday2"]
 			# lg = LineGraph(x_axis, "After market strategy")
 
