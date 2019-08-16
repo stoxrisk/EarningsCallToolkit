73ad2cc0fe3ef959de245f9b064bac7e7bc6d756
jackoaw <worldchuba@gmail.com>
2019-08-16 13:12:13 -0400

    saving code

--- analyze.py
+++ analyze.py
@@ -2,6 +2,7 @@ from yahoofinancials import YahooFinancials
 from tdameritradeapi import TDAmeritradeAPI
 from calendarparser import CalendarParser
 from datetime import timedelta, date
+import matplotlib.pyplot as plts
 import requests
 import os
 
@@ -9,41 +10,66 @@ api_key = os.getenv('td_api_key')
 
 # Return info as map
 
-earnings_cache = {}
+class LineGraph:
+	def __init__(x_axis, title):
+		self.x_axis = x_axis
+		self.title = title
 
-class EarningsData:
+		self.x_len = len(x_axis)
 
-	def __init__(self, timeframe, ticker, numEarningsCalls):
-		self.timeframe = timeframe
-		self.ticker = ticker
+	def addLine(symbol, data ,color):
+		if len(data) == self.x_len:
+			plt.plot(y_axis, data, color='g')
 
-	def dayBefore(self):
-		pass
+	def show():
+		plt.show()
 
-	def dayBeforeChange(self):
-		pass
+# ENTER: TSLA;DAY1-DAY2;9:30-16,16-20|4-9:30,9:30-16;30m
+# RETURN: [1,2,2,2]
+class PriceDifferenceRetriever:
+	def __init__(self, time_str):
+		self.retrieveDay = __timeStringParser(time_str)
 
-	def afterHours(self):
-		pass
+	def __timeStringParser(self, time_str):
+		days_times = time_str.split(";")
+		timeframe = days_times[1].split("-")
+		start_date = datetime.strptime(timeframe[0], '%Y%m%d')
+		end_date = datetime.strptime(timeframe[1], '%Y%m%d')
+		end_date = end_date + datetime.timedelta(days=1) # last day is not inclusive
+		intraday_timeframes = days_times[2].split("|")
+		for daydata in days:
+			getPrices
 
-	def afterHoursChange(self):
-		pass
 
-	def dayAfter(self):
-		pass
+def afterMarketCallDifferenceStrategy(date):
 
-	def dayAfterChange(self):
-		pass
 
-class Strategy1():
-	def __init__():
-		pass
+def beforeMarketCallDifferenceStrategy(date):
+	day_before = date - 1 
+	"TSLA;%s-%s-%s;9:30-20|4-9:30,9:30-16,16-20|4-9:30,9:30-16;30m"
+
+def 
+# This strategy is to simply compare during day movement, after market movement, and 
+# before market movement the next day for earnings calls after market hours.
+# Then compare it against intraday ,movement the next day, once at 12pm and once at 4pm
+# The output of the data will look like this:
+# TSLA 2/5/19
+# Change during the day: 5%
+# Change after hours: -16%
+# Change before hours: -2%
+# Change next day: -2%
+def Strategy1():
+	["intraday", "post-market", "pre-market", "intra-day"]
 
 start_date = date(2018, 6, 13)
 end_date = date(2019, 8, 14)
 cp = CalendarParser("white_list.txt","C:\\\\Users\\Andrew\\Google Drive\\Dropbox\\stox\\EarningsCallToolkit\\dates", start_date, end_date)
 cp.loadCached(True)
-print(cp.earnings_map['AAAP'])
+print(cp.earnings_map['AAPL'])
+
+#TIME FRAME: "DAY:(9:30-16,16-20);DAY:(4-9:30,9:30-16)"
+# "DIFFERENCE"
+	
 
 # Desired return output
 # ---------------------------- 
