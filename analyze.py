f5e379039e530dc344b3b0bd99792cee3b3aa329
jackoaw <worldchuba@gmail.com>
2019-08-20 16:21:08 -0400

    acceptable condition

--- analyze.py
+++ analyze.py
@@ -3,10 +3,10 @@ from calendarparser import CalendarParser
 from datetime import timedelta, date, datetime
 import pytz
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import requests
 import os
 import json
-from time import sleep
 
 api_key = os.getenv('td_api_key')
 
@@ -15,14 +15,25 @@ class LineGraph:
 	def __init__(self, x_axis, title):
 		self.x_axis = x_axis
 		self.title = title
-
+		self.color = "C0"
 		self.x_len = len(x_axis)
+
+		mpl.style.use('seaborn')
+
+	def changeColor(self):
+		old_color = int(self.color[1])
+		print(old_color)
+		print(str(old_color + 1))
+		self.color = "C" + str(old_color + 1)
+		print(self.color)
+		
 
 	def plot(self, symbol, diff_array, x_axis, color):
 		if len(diff_array) == self.x_len:
-			plt.plot(x_axis, diff_array, color='g')
+			plt.plot(x_axis, diff_array, color=self.color, label=symbol)
 
 	def show(self):
+		plt.legend()
 		plt.show()
 
 
@@ -47,7 +58,7 @@ class IntraDayToolkit:
 		
 	# Takes in datetimes, times as strings, ticker, candlestick type
 	# Returns: Candles for the beginning and ends of the times
-	def retrieveSelectedTimesDifferences(self, dates, times, ticker, candlestick):
+	def retrieveSelectedTimes(self, dates, times, ticker, candlestick, req_data_length):
 		# Only processesing for the year 2019 right now, because the only data I can seem to get from TDAmeritrade is 2019+
 		if str(dates[0].year) != "2019":
 			return False
@@ -76,25 +87,29 @@ class IntraDayToolkit:
 			# print("Looking for: %s" % milisecond_list[h])
 			# print("Comparing %s and %s"%(candle["datetime"], milisecond_list[h]))
 			if int(candle["datetime"]) == milisecond_list[h]:
+				# print("found!")
 				close_list.append(candle["close"])
 				h += 1
 				if h == len(milisecond_list):
 					break
+
+		if req_data_length != len(close_list):
+			raise Exception("Invalid Data length retrieved")
+
 		return close_list
 
 	# Instructions will be a list used to identify which indicies to compare
 	# [157.3247, 155.79, 163.02, 163.0, 160.7784, 165.25] , ["1:2", "2:3", "4:5", "5:4"]
 	def priceArrayToDifferenceArray(self, price_array, instructions):
 		diff_array = []
+		# print(price_array)
 		# Start off with 0 change for a more readable line chart
 		diff_array.append(0)
 		for instruction in instructions:
-			print('lll')
 			i_list = instruction.split(":")
 			close1 = price_array[int(i_list[0])]
 			close2 = price_array[int(i_list[1])] 
 			diff = ((close2 - close1)/close1)*100
-			print(diff)
 			diff_array.append(diff)
 
 		return diff_array
@@ -105,16 +120,19 @@ class IntraDayToolkit:
 def afterMarketCallDifferenceStrategy(earnings_map, ticker_list):
 	tk = IntraDayToolkit(api_key)
 	# 7 to adjust for TD Ameritrade api, earliest data available
-	after_market_strat_times = ["9:30,16,19", "4,9:30,16"]
+	after_market_strat_times = ["9:30,16,19", "5,9:30,16"]
 	# We can use a graph like this to look for a pattern of upward or downward slopes to buy in at the right times
 	difference_instructions =  ["0:1", "1:2", "3:4", "4:5"]
 	# We can use this graph for the simplest viewing experience, comparing vs. a given day
 	# difference_instructions_s =  ["1:2", "1:3", "4:5", "5:4"]
+	x_axis = ["9:30 AM", "9:30 AM - 4 PM", "4 PM - 7:00 PM", "4 AM - 9:30 AM", "9:30 AM - 4:00 PM"]
+	lg = LineGraph(x_axis, "After market strategy")
 	for ticker in ticker_list:
+		print(ticker)
 		earnings_date_list = cp.earnings_map[ticker]
-		x_axis = ["9:30 AM", "9:30 AM - 4 PM", "4 PM - 7:00 PM", "4 AM - 9:30 AM", "9:30 AM - 4:00 PM"]
-		lg = LineGraph(x_axis, "After market strategy")
+		lg.changeColor()
 		for earningsdate in earnings_date_list:
+			# print(earningsdate)
 			dateandtime = earningsdate.split(":")
 			# We only want to pay attention to stocks with after market close
 			try:
@@ -131,27 +149,17 @@ def afterMarketCallDifferenceStrategy(earnings_map, ticker_list):
 			end_date = start_date + timedelta(days=1, hours=20) # last day is not inclusive
 			dates = [start_date, end_date]
 			# Expected return [1.1,2.1,-1,2] in percentages
-			close_list = tk.retrieveSelectedTimesDifferences(dates, after_market_strat_times, ticker, "30m")
+			close_list = tk.retrieveSelectedTimes(dates, after_market_strat_times, ticker, "30m", 6)
 			if(close_list):
 				diff_array = tk.priceArrayToDifferenceArray(close_list, difference_instructions)
-				print(close_list)
+				# print(close_list)
 				lg.plot(ticker, diff_array, x_axis,'g')
-		lg.show()
+		
+	lg.show()
 
 
 
 cp = CalendarParser("white_list.txt","C:\\\\Users\\Andrew\\Google Drive\\Dropbox\\stox\\EarningsCallToolkit\\dates")
 cp.loadCached(False)
 print(cp.earnings_map['AAPL'])
-afterMarketCallDifferenceStrategy(cp.earnings_map, ["AAPL"])
-
-
-# This strategy is to simply compare during day movement, after market movement, and 
-# before market movement the next day for earnings calls after market hours.
-# Then compare it against intraday ,movement the next day, once at 12pm and once at 4pm
-# The output of the data will look like this:
-# TSLA 2/5/19
-# Change during the day: 5%
-# Change after hours: -16%
-# Change before hours: -2%
-# Change next day: -2%
+afterMarketCallDifferenceStrategy(cp.earnings_map, ["AMZN", "MSFT"])
