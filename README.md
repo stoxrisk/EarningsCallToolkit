cca8385dfb1ed0231c578561215a3a5cd0bf2204
jackoaw <worldchuba@gmail.com>
2019-10-07 11:19:27 -0400

    additional functionality added to estimating strikes

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 The purpose of this library is to provide tools that will allow you to analyze prices around earnings dates (before, after, during) and judge and visualize the data. It comes with strategies that I am using to make my own market decisions.
 
-Current Stage: [In-Development] 
+Currently there are two strategies, one of them is for analyzing intraday earnings call data for after market earnings calls, and one of them is for analyzing earnings calls on a daily time frame and that data is used to get an overview of past earnings calls data for a particular symbol. 
+
+Current Stage: Beta 
 
 ## Installation:
 
@@ -12,13 +14,17 @@ pip install -r requirements.txt
 
 python analyze.py
 
+## Usage:
+
+
+
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
 ## TODO:
 - Before Market Strategy
-- Refactoring for including more strategies
 - Instructions Parser needs to be more flexible
+- Additional Module Pulling latest market data times (new ones are added daily)
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- estimate_strikes.py
+++ estimate_strikes.py
@@ -2,21 +2,25 @@ import sys
 import os 
 from datetime import timedelta, date, datetime
 from yahoofinancials import YahooFinancials
-# Code for gathering the latest day
-latest_day = date.today() - timedelta(days=1)
-if latest_day.strftime('%A') == 'Saturday':
-    latest_day = latest_day - timedelta(days=3)
-elif latest_day.strftime('%A') == 'Sunday':
-    latest_day = latest_day - timedelta(days=3)
-latest_day_str = latest_day.strftime('%Y-%m-%d') 
-day_before_latest = latest_day - timedelta(days=1)
-if day_before_latest.strftime('%A') == "Sunday":
-    day_before_latest = day_before_latest - timedelta(days=2)
-day_before_latest_str = day_before_latest.strftime('%Y-%m-%d')
-symbol = sys.argv[1]
 
-symbol_yahoo_finance_interactor = YahooFinancials(symbol)
-earnings_days_data = symbol_yahoo_finance_interactor.get_historical_price_data(day_before_latest_str, latest_day_str, 'daily')
+symbol = sys.argv[1]
+try:
+    current_price = float(sys.argv[2])
+except:
+    # Code for gathering the latest day
+    latest_day = date.today() - timedelta(days=1)
+    if latest_day.strftime('%A') == 'Saturday':
+        latest_day = latest_day - timedelta(days=3)
+    elif latest_day.strftime('%A') == 'Sunday':
+        latest_day = latest_day - timedelta(days=3)
+    latest_day_str = latest_day.strftime('%Y-%m-%d') 
+    day_before_latest = latest_day - timedelta(days=1)
+    if day_before_latest.strftime('%A') == "Sunday":
+        day_before_latest = day_before_latest - timedelta(days=2)
+    day_before_latest_str = day_before_latest.strftime('%Y-%m-%d')
+    symbol_yahoo_finance_interactor = YahooFinancials(symbol)
+    earnings_days_data = symbol_yahoo_finance_interactor.get_historical_price_data(day_before_latest_str, latest_day_str, 'daily')
+    current_price = earnings_days_data[symbol]['prices'][0]['close']
 
 symbol_file = os.getcwd() + "\\earnings_call_difference_data\\%s.csv"%symbol
 csv_file = open(symbol_file, "r") 
@@ -48,7 +52,7 @@ print("Positive Difference Average: %f%%"% positive_diff)
 print("Negative Difference Average: %f%%"% negative_diff)
 print("-----------------------------------------")
 # print(earnings_days_data)
-current_price = earnings_days_data[symbol]['prices'][0]['close']
+
 
 print("Current Price: %f"%current_price)
 lower_strike = (round((current_price - ((abs(negative_diff)/100)*current_price))*2))/2
