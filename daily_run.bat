bc768017d602d90a371870760544874eef32f923
jackoaw <jackoaw@vt.edu>
2019-12-15 20:57:28 -0500

    Daily Update

--- daily_run.bat
+++ daily_run.bat
@@ -1,1 +1,1 @@
-C:\Users\Andrew\Google Drive\Dropbox\stox\venv\Scripts\activate & cd C:\Users\Andrew\Google Drive\Dropbox\stox\EarningsCallToolkit & python daily_run.py & git add . & git commit -m "Daily Update" & git push
+C:\Users\Andrew\"Google Drive"\Dropbox\stox\venv\Scripts\activate & cd C:\Users\Andrew\"Google Drive"\Dropbox\stox\EarningsCallToolkit & python daily_run.py & git add . & git commit -m "Daily Update" & git push
