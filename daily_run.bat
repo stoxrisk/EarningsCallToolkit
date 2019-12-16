27b3dbe7b386428accab07a979d9f318d3c58c21
jackoaw <jackoaw@vt.edu>
2019-12-15 19:57:08 -0500

    Daily Update

--- daily_run.bat
+++ daily_run.bat
@@ -1,4 +1,1 @@
-cd .. & venv/Scripts/activate & cd EarningsCallToolkit & python daily_run.py
-git add .
-git commit -m "Daily Update" 
-git push
+cd .. & venv/Scripts/activate & cd EarningsCallToolkit & python daily_run.py & git add . & git commit -m "Daily Update" & git push
