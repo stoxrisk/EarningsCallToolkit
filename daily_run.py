c8f9d74f9ec2a1d4514f1f79d67bf9eda9eb169a
jackoaw <jackoaw@vt.edu>
2019-12-15 20:52:16 -0500

    Daily Update

--- daily_run.py
+++ daily_run.py
@@ -9,7 +9,7 @@ import subprocess
 def start():
     # todaysdate = "20191211"
     todaysdate = datetime.now()
-    todaysdate = todaysdate.replace(day=5)
+    todaysdate = todaysdate.replace(day=11)
     todays_string = todaysdate.strftime("%Y%m%d")
     # First update the data 
 
